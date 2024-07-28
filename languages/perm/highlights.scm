e60f4d7356011ac66d8e80314aa0f557ad6a724b
Steven Turturo <steven@avocagrow.com>
2024-07-28 11:06:53 -0700

    feat: basic perm highlighting working

--- languages/perm/highlights.scm
+++ languages/perm/highlights.scm
@@ -1,11 +1,18 @@
 [
   "entity"
   "relation"
+  "action"
+  "permission"
 ]@keyword
 
 [
   (and_statement)
   (or_statement)
+  (not_statement)
+  (in_statement)
 ]@keyword.conditional
 
 (comment) @comment
+
+(relation_association) @type
+(relation_member) @type.builtin
